deb3f97724984ef66148ac81592571abbd429784
Dan Sudol <dsudol@medivo.com>
2012-01-27 13:38:20 -0500

    removing app assets from public

--- app/assets/javascripts/consumer/mobile/application.js
+++ app/assets/javascripts/consumer/mobile/application.js
@@ -1,13 +1,13 @@
 // For the consumer side of the application
-//= require jquery
-//= require support/jquery.mobile
-//= require support/handlebars
-//= require support/underscore
-//= require support/date
-//= require support/backbone
-//= require support/has_class_plugin
-//= require map
-//= require consumer/models
-//= require consumer/views
-//= require consumer/handlers
-//= require consumer/mobile/mobile_view
+// require jquery
+// require support/jquery.mobile
+// require support/handlebars
+// require support/underscore
+// require support/date
+// require support/backbone
+// require support/has_class_plugin
+// require map
+// require consumer/models
+// require consumer/views
+// require consumer/handlers
+// require consumer/mobile/mobile_view
